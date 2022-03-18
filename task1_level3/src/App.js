2d07247802001756268a084e5fb4512f3ce7aa7d
Rakhymbek Sagyn <sagrakha@gmail.com>
2022-03-18 14:00:41 +0600

    feat: implement sort function

--- task1_level3/src/App.js
+++ task1_level3/src/App.js
@@ -1,9 +1,50 @@
+import tableData from './data';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const copyData = [...tableData];
+  const [data, setData] = useState(copyData);
+  const [sortType, setSortType] = useState('default');
+
+  const sortByPrice = () => {
+    if(sortType === 'default') {
+      setSortType('desc');
+      setData(copyData.sort((a, b) => parseInt(b.price.replace(/ /g, '')) - parseInt(a.price.replace(/ /g, ''))));
+    }else if(sortType === 'desc') {
+      setSortType('asc');
+      setData(copyData.sort((a, b) => parseInt(a.price.replace(/ /g, '')) - parseInt(b.price.replace(/ /g, ''))));
+    }else if(sortType === 'asc') {
+      setSortType('default');
+      setData(tableData);
+    }
+}
+
+  
   return (
-    <div className="App">
-      
+    <div className='App'>
+      <table>
+        <thead>
+          <tr>
+            <th>№</th>
+            <th>Название</th>
+            <th><button className='price_btn'  onClick={sortByPrice}>Цена<span className="icons descending_icon">&nbsp;{(sortType === 'default' || sortType === 'desc') ? '↓' : '🔄'}</span></button></th>
+            <th><button className='price_btn'>Количество</button></th>
+            <th>В рассрочку</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item, index) => (
+            <tr key={index}>
+              <td>{index + 1}</td>
+              <td>{item.name}</td>
+              <td>{item.price}</td>
+              <td>{item.count}</td>
+              <td>{(item.instalment) ? '✅' : ''}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
